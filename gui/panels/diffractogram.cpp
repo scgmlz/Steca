0250ca187cbb97a48b9a14528368a1c8b9d446f8
Jan Burle <jan@c53.be>
2016-01-21 11:43:53 +0100

    Plot has a cursor

--- gui/panels/diffractogram.cpp
+++ gui/panels/diffractogram.cpp
@@ -1,11 +1,60 @@
 #include "diffractogram.h"
-#include "../3rd/qcustomplot.h"
+#include <QPainter>
+#include <QLineF>
 
 namespace panel {
 
+DiffractogramPlotOverlay::DiffractogramPlotOverlay(DiffractogramPlot& plot)
+  : super(&plot), hasCursor(false), cursorPos(0) {
+  setMouseTracking(true);
+}
+
+void DiffractogramPlotOverlay::enterEvent(QEvent*) {
+  hasCursor = true;
+  updateCursorRegion();
+}
+
+void DiffractogramPlotOverlay::leaveEvent(QEvent*) {
+  hasCursor = false;
+  updateCursorRegion();
+}
+
+void DiffractogramPlotOverlay::mouseMoveEvent(QMouseEvent* e) {
+  updateCursorRegion();
+  cursorPos = e->x();
+  updateCursorRegion();
+}
+
+void DiffractogramPlotOverlay::paintEvent(QPaintEvent*) {
+  QPainter painter(this);
+  auto g = geometry();
+
+  if (hasCursor) {
+    QLineF cursor(cursorPos,g.top(),cursorPos,g.bottom());
+
+    painter.setPen(Qt::red);
+    painter.drawLine(cursor);
+  }
+}
+
+void DiffractogramPlotOverlay::updateCursorRegion() {
+  auto g = geometry();
+  update(cursorPos,g.top(),1,g.height());
+}
+
+DiffractogramPlot::DiffractogramPlot() {
+  overlay = new DiffractogramPlotOverlay(*this);
+}
+
+void DiffractogramPlot::resizeEvent(QResizeEvent* e) {
+  super::resizeEvent(e);
+  auto size = e->size();
+  overlay->setGeometry(0,0,size.width(),size.height());
+}
+
 Diffractogram::Diffractogram(MainWin& mainWin,Session& session)
 : super("Diffractogram",mainWin,session,Qt::Vertical) {
-  box->addWidget(new QCustomPlot());
+  box->addWidget(new DiffractogramPlot);
   box->addWidget(check("From all images"));
 }
 
--- gui/panels/diffractogram.h
+++ gui/panels/diffractogram.h
@@ -2,8 +2,41 @@
 #define DIFFRACTOGRAM_H
 
 #include "panel.h"
+#include "../3rd/qcustomplot.h"
 
 namespace panel {
+
+class DiffractogramPlot;
+
+class DiffractogramPlotOverlay: public QWidget {
+  SUPER(DiffractogramPlotOverlay,QWidget)
+public:
+  DiffractogramPlotOverlay(DiffractogramPlot&);
+
+protected:
+  void enterEvent(QEvent*);
+  void leaveEvent(QEvent*);
+  void mouseMoveEvent(QMouseEvent*);
+
+  void paintEvent(QPaintEvent*);
+
+  bool  hasCursor;
+  int   cursorPos;
+
+  void updateCursorRegion();
+};
+
+class DiffractogramPlot: public QCustomPlot {
+  SUPER(DiffractogramPlot,QCustomPlot)
+public:
+  DiffractogramPlot();
+
+protected:
+  void resizeEvent(QResizeEvent*);
+
+private:
+  DiffractogramPlotOverlay *overlay;
+};
 
 class Diffractogram: public BoxPanel {
   SUPER(Diffractogram,BoxPanel)
