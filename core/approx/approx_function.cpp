d9208c77512617d127ed44b7098515097e12f4ae
Jan Burle <jan@c53.be>
2016-02-16 15:23:26 +0100

    better message in QT verison check

--- core/approx/approx_function.cpp
+++ core/approx/approx_function.cpp
@@ -2,6 +2,67 @@
 
 namespace core { namespace approx {
 
+Function::Parameter::Parameter(): value(0), range() {
+}
+
+qreal Function::Parameter::getValue() const {
+  return value;
+}
+
+Range Function::Parameter::getRange() const {
+  return range.isValid() ? range : Range(value);
+}
+
+bool Function::Parameter::checkValue(qreal v) {
+  if (range.isValid()) {
+    if (!range.contains(v)) return false;
+  }
+
+  return true;
+}
+
+bool Function::Parameter::setValue(qreal value_, bool force) {
+  if (force || checkValue(value_)) {
+    value = value_;
+    return true;
+  }
+
+  return false;
+}
+
+Function::Function() {
+}
+
+Function::~Function()
+{
+}
+
+void Function::setParameterCount(uint n) {
+  parameters.fill(Parameter(),n);
+}
+
+uint Function::getParameterCount() const {
+  return parameters.count();
+}
+
+Functions::Functions() {
+}
+
+Functions::~Functions() {
+  for (auto f: functions)
+    delete f;
+}
+
+SumFunctions::SumFunctions() {
+}
+
+qreal SumFunctions::y(qreal x) const {
+  qreal sum = 0;
+  for (Function *f: functions.getFunctions())
+    sum += f->y(x);
+  return sum;
+}
+
 }}
 
 // eof
--- core/approx/approx_function.h
+++ core/approx/approx_function.h
@@ -1,7 +1,76 @@
 #ifndef APPROX_FUNCTION_H
 #define APPROX_FUNCTION_H
 
+#include <core_lib.h>
+
 namespace core { namespace approx {
+
+class Function {
+public:
+  class Parameter final {
+  public:
+    Parameter();
+
+    qreal getValue() const;
+    Range getRange() const;
+
+    bool  checkValue(qreal);
+    bool  setValue(qreal, bool force=false);
+
+  private:
+    qreal value;
+    /// Constraint; if !isValid() -> same as <value,value>
+    Range range;
+  };
+
+public:
+  Function();
+  virtual ~Function();
+
+  typedef QVector<Parameter> parameters_t;
+
+  void setParameterCount(uint);
+  uint getParameterCount() const;
+
+  parameters_t& getParameters() {
+    return parameters;
+  }
+
+  parameters_t const& getParameters() const {
+    return const_cast<Function*>(this)->getParameters();
+  }
+
+  virtual qreal y(qreal x) const = 0;
+
+protected:
+  parameters_t parameters;
+};
+
+class Functions final {
+public:
+  Functions();
+ ~Functions();
+
+  typedef QVector<Function*> functions_t;
+
+  functions_t const& getFunctions() const {
+    return functions;
+  }
+
+private:
+  functions_t functions;
+};
+
+class SumFunctions: public Function {
+  SUPER(SumFunctions,Function)
+public:
+  SumFunctions();
+
+  qreal y(qreal x) const;
+
+protected:
+  Functions functions;
+};
 
 }}
 
