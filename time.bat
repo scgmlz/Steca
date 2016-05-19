4a606fea75c9affdad7eb4cd2060e700d792a0f4
Rebecca Brydon <rebecca.elisabeth@outlook.com>
2016-05-19 12:46:38 +0200

    bat file for renaming steca2.exe with date and time stamp

---
+++ time.bat
@@ -0,0 +1,4 @@
+@echo off
+For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+ren STeCa2.exe STeCa2_%mydate%_%mytime%.exe
