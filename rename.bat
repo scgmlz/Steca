9a02115a91378a94f30f780d33d56674c972ee27
Rebecca Brydon <rebecca.elisabeth@outlook.com>
2016-05-20 09:38:09 +0200

    for windows build

--- rename.bat
+++ rename.bat
@@ -2,3 +2,4 @@
 For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 ren build.exe STeCa2_%mydate%_%mytime%.exe
+copy STeCa2_%mydate%_%mytime%.exe F:\STeCa2_%mydate%_%mytime%.exe
