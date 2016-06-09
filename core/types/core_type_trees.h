219a94e2bba0b47431d76cf4f7795e8f1a76e5e7
Rebecca Brydon <rebecca.elisabeth@outlook.com>
2016-06-09 12:23:10 +0200

    This is release 2.0.0.1 with hotfix for icon and authors names

--- core/types/core_type_trees.h
+++ core/types/core_type_trees.h
@@ -9,7 +9,7 @@
 //! @license   GNU General Public License v3 or higher (see COPYING)
 //! @copyright Forschungszentrum Jülich GmbH 2016
 //! @authors   Scientific Computing Group at MLZ Garching
-//! @authors   Antti Soininen, Jan Burle, Rebecca Brydon
+//! @authors   Rebecca Brydon, Jan Burle,  Antti Soininen
 //! @authors   Based on the original STeCa by Christian Randau
 //
 // ************************************************************************** //
