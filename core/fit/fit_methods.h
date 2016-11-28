ba86942e463e011521b3bfa1e25ab7bcd40060b1
Jan Burle <jan@c53.be>
2016-11-28 14:11:59 +0100

    changed boilerplate header, particulars moved into AUTHORS

--- core/fit/fit_methods.h
+++ core/fit/fit_methods.h
@@ -1,17 +1,19 @@
 // ************************************************************************** //
 //
-//  STeCa2:    StressTextureCalculator ver. 2
+// STeCa2 - StressTextureCalculator ver. 2
 //
-//! @file      fit_methods.h
-//! @brief     Fitting methods
-//!
-//! @homepage  http://apps.jcns.fz-juelich.de/steca2
-//! @license   GNU General Public License v3 or higher (see COPYING)
-//! @copyright Forschungszentrum Jülich GmbH 2016
-//! @authors   Scientific Computing Group at MLZ Garching
-//! @authors   Rebecca Brydon, Jan Burle, Antti Soininen
-//! @authors   Based on the original STeCa by Christian Randau
+// Copyright (C) 2016 Forschungszentrum Jülich GmbH 2016
 //
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// See the COPYING and AUTHORS files for more details.
 // ************************************************************************** //
 
 #ifndef FIT_METHODS_H
