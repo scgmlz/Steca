67dee6157e6775f55cbab839dcbf3358568af6a6
Jan Burle <jan@c53.be>
2016-12-14 14:33:44 +0100

    Update build.sh

--- .travis/linux/build.sh
+++ .travis/linux/build.sh
@@ -4,5 +4,5 @@ export PATH=$QTDIR/bin:$PATH
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE ..
+cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_TESTS=1 ..
 make -j9
