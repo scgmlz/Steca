6594e02c0eef99acc8bce1c0934a93d8baa095d9
Jan Burle <jan@c53.be>
2016-12-14 17:03:49 +0100

    Update test_Debug.sh

--- .travis/linux/test_Debug.sh
+++ .travis/linux/test_Debug.sh
@@ -1,3 +1,6 @@
 cd build
 xvfb-run tests/tests
-lcov -c -d .
+lcov -c -d . -o coverage.info
+lcov -r coverage.info 'tests/*' -o coverage.info
+lcov -l coverage.info
+coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
