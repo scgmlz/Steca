1511e4499f705a05a86fc016a457f624389b282d
Jan Georg Burle <jan@c53.be>
2017-08-01 11:47:43 +0200

    Update test_Debug.sh

--- .travis/linux/test_Debug.sh
+++ .travis/linux/test_Debug.sh
@@ -3,4 +3,4 @@ xvfb-run tests/tests
 lcov -c -d . -o coverage.info
 lcov -r coverage.info 'tests/*' '/opt/*' '/usr/*' -o coverage.info
 lcov -l coverage.info
-coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+coveralls-lcov -t ${COVERALLS_TOKEN} coverage.info
