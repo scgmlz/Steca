33bde78c26a3d8a7d4c47cf36e8692c87189cce4
Jan Burle <jan@c53.be>
2017-12-05 10:57:41 +0100

    removed lcov

--- .travis/linux/test_Debug.sh
+++ .travis/linux/test_Debug.sh
@@ -1,6 +1,2 @@
 cd build
 xvfb-run tests/tests
-lcov -c -d . -o coverage.info
-lcov -r coverage.info 'tests/*' '/opt/*' '/usr/*' -o coverage.info
-lcov -l coverage.info
-coveralls-lcov -t ${COVERALLS_TOKEN} coverage.info
