beed6141b8ebc52d956ae313125d5d99e4416311
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2017-12-07 13:55:04 +0100

    adapt win make

--- make_pro.sh
+++ make_pro.sh
@@ -17,7 +17,7 @@ win32 {
   RC_ICONS = \$\$PWD/gui/icons/retro_stier.ico
 }
 
-INCLUDEPATH += \$\$PWD/3rdparty/LevMar \$\$PWD/3rdparty/Mar \$\$PWD/3rdparty/Caress \$\$PWD/3rdparty/QCustomPlot \$\$PWD/3rdparty/doctest \$\$PWD/lib \$\$PWD/io \$\$PWD/core \$\$PWD/gui
+INCLUDEPATH += \$\$PWD/3rdparty/googletest \$\$PWD/3rdparty/LevMar \$\$PWD/3rdparty/Mar \$\$PWD/3rdparty/Caress \$\$PWD/3rdparty/QCustomPlot \$\$PWD/lib \$\$PWD/io \$\$PWD/core \$\$PWD/gui
 
 EOT
 
