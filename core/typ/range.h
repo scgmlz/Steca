dcb1c3bd600dc21a3ef5071a57c88c2c8526f768
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2017-12-19 17:43:28 +0100

    comments

--- core/typ/range.h
+++ core/typ/range.h
@@ -27,41 +27,40 @@ class JsonObj;
 
 //! a range of values - a closed interval
 class Range {
-    public:
+public:
 
-    Range(); // invalid (NaN)
-    Range(qreal val); // singular, min == max
-    Range(qreal min, qreal max); // normal
+    Range(); //!< invalid (NaN)
+    Range(qreal val); //!< singular, min == max
+    Range(qreal min, qreal max); //!< normal
 
-    static Range infinite(); // factory: -inf .. +inf
+    static Range infinite(); //!< factory: -inf .. +inf
 
     COMPARABLE(Range const&)
 
-    void invalidate(); // make invalid
-    bool isValid() const; // is not NaN
-    bool isEmpty() const; // not valid or empty
+    void invalidate(); //!< make invalid
+    bool isValid() const; //!< is not NaN
+    bool isEmpty() const; //!< not valid or empty
 
     qreal width() const;
     qreal center() const;
 
     qreal min, max; // this is the range
 
-    void set(qreal val); // make singular
-    void set(qreal min, qreal max); // must be: min <= max
-    void safeSet(qreal, qreal); // will be set in the right order min/max
+    void set(qreal val); //!< make singular
+    void set(qreal min, qreal max); //!< must be: min <= max
+    void safeSet(qreal, qreal); //!< will be set in the right order min/max
 
-    static Range safeFrom(qreal, qreal); // safe factory
+    static Range safeFrom(qreal, qreal); //!< safe factory
 
-    void extendBy(qreal); // extend to include the number
-    void extendBy(Range const&); // extend to include the range
+    void extendBy(qreal); //!< extend to include the number
+    void extendBy(Range const&); //!< extend to include the range
 
     bool contains(qreal) const;
     bool contains(Range const&) const;
     bool intersects(Range const&) const;
     Range intersect(Range const&) const;
 
-    // limit the number to the interval, as qBound would
-    qreal bound(qreal) const;
+    qreal bound(qreal) const; //!< limit the number to the interval, as qBound would
 
     JsonObj to_json() const;
     void from_json(JsonObj const&) THROWS;
@@ -69,7 +68,7 @@ class Range {
 
 //! A set of *sorted* *non-overlapping* ranges
 class Ranges {
-    public:
+public:
     Ranges();
 
     void clear() { ranges_.clear(); }
@@ -78,19 +77,16 @@ class Ranges {
 
     Range const& at(uint i) const { return ranges_.at(i); }
 
-    // collapses overlapping ranges into one; returns whether there was a change
-    bool add(Range const&);
+    bool add(Range const&); //!< collapses overlapping ranges; returns true if *this changed
 
-    // removes (cuts out) a range; returns whether there was a change
-    bool rem(Range const&);
+    bool rem(Range const&); //!< removes (cuts out) a range; returns whether there was a change
+
+    QJsonArray to_json() const;
+    void from_json(QJsonArray const&) THROWS;
 
 private:
     void sort();
     vec<Range> ranges_;
-
-public:
-    QJsonArray to_json() const;
-    void from_json(QJsonArray const&) THROWS;
 };
 
 } // namespace typ
