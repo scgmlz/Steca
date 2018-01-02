07de04dadd1fff2068fbdc715b6253bb5ccb0fa5
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-01-02 18:23:28 +0100

    com

--- gui/panels/tabs_diffractogram.cpp
+++ gui/panels/tabs_diffractogram.cpp
@@ -3,7 +3,7 @@
 //  Steca2: stress and texture calculator
 //
 //! @file      gui/panels/tabs_diffractogram.cpp
-//! @brief     Implements ...
+//! @brief     Implements class TabsDiffractogram
 //!
 //! @homepage  https://github.com/scgmlz/Steca2
 //! @license   GNU General Public License v3 or higher (see COPYING)
