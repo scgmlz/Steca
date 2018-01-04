scgmlz/Steca

commit 75b607f0ac2cd84ec23df5e264d245ee1cc64eba

simplify file loading, try to corr win failure

core/io/io_io.cpp
@@ -15,12 +15,16 @@
 #include "data/datafile.h"
 #include "typ/exception.h"
 #include <QStringBuilder> // for ".." % ..
+#include <QFileInfo>
 
 namespace io {
+Datafile loadCaress(rcstr filePath) THROWS;
+Datafile loadMar(rcstr filePath) THROWS;
+Datafile loadTiffDat(rcstr filePath) THROWS;
+str loadCaressComment(rcstr filePath);
+}
 
-QSharedPointer<Datafile const> loadCaress(rcstr filePath) THROWS;
-QSharedPointer<Datafile const> loadMar(rcstr filePath) THROWS;
-QSharedPointer<Datafile const> loadTiffDat(rcstr filePath) THROWS;
+namespace {
 
 // peek at up to maxLen bytes (to establish the file type)
 static QByteArray peek(uint pos, uint maxLen, QFileInfo const& info) {
@@ -76,23 +80,40 @@ bool couldBeTiffDat(QFileInfo const& info) {
     return couldBe;
 }
 
-QSharedPointer<const Datafile> load(rcstr filePath) THROWS {
+Datafile load_low_level(rcstr filePath) THROWS {
     QFileInfo info(filePath);
     RUNTIME_CHECK(info.exists(), "File " % filePath % " does not exist");
 
-    QSharedPointer<const Datafile> file;
     if (couldBeCaress(info))
-        file = io::loadCaress(filePath);
+        return io::loadCaress(filePath);
     else if (couldBeMar(info))
-        file = io::loadMar(filePath);
+        return io::loadMar(filePath);
     else if (couldBeTiffDat(info))
-        file = io::loadTiffDat(filePath);
+        return io::loadTiffDat(filePath);
     else
         THROW("unknown file type: " % filePath);
+}
 
-    RUNTIME_CHECK(file->suite().count() > 0, "File " % filePath % " contains no suite");
+} // anonymous namespace
 
-    return file;
+namespace io {
+
+QSharedPointer<Datafile const> loadDatafile(rcstr filePath) THROWS {
+    auto ret = QSharedPointer<Datafile const>(new Datafile(load_low_level(filePath)));
+    RUNTIME_CHECK(ret->suite().count() > 0, "File " % filePath % " contains no suite");
+    return ret;
+}
+
+str loadComment(QFileInfo const& info) {
+    auto path = info.absoluteFilePath();
+    if (couldBeCaress(info))
+        return "[car] " + io::loadCaressComment(path);
+    else if (couldBeMar(info))
+        return "[mar] ";
+    else if (couldBeTiffDat(info))
+        return "[tif] ";
+    else
+        NEVER;
 }
 
 } // namespace io

core/io/io_io.h
@@ -24,14 +24,11 @@ class QFileInfo;
 
 namespace io {
 
-bool couldBeCaress(QFileInfo const&);
-bool couldBeMar(QFileInfo const&);
-bool couldBeTiffDat(QFileInfo const&);
-
 //! load a file; file type will be sensed
-QSharedPointer<const Datafile> load(rcstr filePath) THROWS;
+QSharedPointer<Datafile const> loadDatafile(rcstr filePath) THROWS;
 
 str loadCaressComment(rcstr filePath);
+str loadComment(QFileInfo const& info);
 
 } // namespace io
 

core/io/io_caress.cpp
@@ -24,8 +24,8 @@
 
 namespace io {
 
-QSharedPointer<Datafile> loadCaress(rcstr filePath) THROWS {
-    QSharedPointer<Datafile> datafile(new Datafile(filePath));
+Datafile loadCaress(rcstr filePath) THROWS {
+    Datafile ret(filePath);
 
     RUNTIME_CHECK(
         0 == open_data_file(filePath.toLocal8Bit().data(), nullptr),
@@ -208,7 +208,7 @@ QSharedPointer<Datafile> loadCaress(rcstr filePath) THROWS {
                 md.deltaTime = deltaTime;
                 md.time = tempTime;
 
-                datafile->addDataset(md, size, convertedIntens);
+                ret.addDataset(md, size, convertedIntens);
 
                 delete[] intens;
                 intens = NULL;
@@ -553,7 +553,7 @@ QSharedPointer<Datafile> loadCaress(rcstr filePath) THROWS {
         }
     }
 
-    return datafile;
+    return ret;
 }
 
 str loadCaressComment(rcstr filePath) {

core/io/io_mar.cpp
@@ -22,10 +22,10 @@ namespace io {
 
 // Code taken from the original STeCa, only slightly modified.
 
-QSharedPointer<Datafile> loadMar(rcstr filePath) THROWS {
+Datafile loadMar(rcstr filePath) THROWS {
     typedef short WORD;
 
-    QSharedPointer<Datafile> datafile(new Datafile(filePath));
+    Datafile ret(filePath);
 
     FILE* fpIn;
 
@@ -219,12 +219,12 @@ QSharedPointer<Datafile> loadMar(rcstr filePath) THROWS {
 
     // REVIEW ?? pictureOverflow
 
-    datafile->addDataset(md, size2d(pixSizeX, pixSizeY), convertedIntens);
+    ret.addDataset(md, size2d(pixSizeX, pixSizeY), convertedIntens);
 
     delete[] i2_image;
     delete[] i4_image;
 
-    return datafile;
+    return ret;
 }
 
 } // namespace io

core/io/io_tiff.cpp
@@ -22,7 +22,7 @@
 namespace io {
 
 // implemented below
-static void loadTiff(QSharedPointer<Datafile>&, rcstr, deg, qreal, qreal) THROWS;
+static void loadTiff(Datafile*, rcstr, deg, qreal, qreal) THROWS;
 
 // The dat file looks like so:
 /*
@@ -41,8 +41,8 @@ Aus-Weimin-00008.tif -55
 Aus-Weimin-00009.tif -50
 */
 
-QSharedPointer<Datafile> loadTiffDat(rcstr filePath) THROWS {
-    QSharedPointer<Datafile> datafile(new Datafile(filePath));
+Datafile loadTiffDat(rcstr filePath) THROWS {
+    Datafile ret(filePath);
 
     QFile f(filePath);
     RUNTIME_CHECK(f.open(QFile::ReadOnly), "cannot open file");
@@ -87,7 +87,7 @@ QSharedPointer<Datafile> loadTiffDat(rcstr filePath) THROWS {
 
         try {
             // load one dataseq
-            loadTiff(datafile, dir.filePath(tiffFileName), phi, monitor, expTime);
+            loadTiff(&ret, dir.filePath(tiffFileName), phi, monitor, expTime);
         } catch (Exception& e) {
             // add file name to the message
             e.setMsg(tiffFileName + ": " + e.msg());
@@ -95,7 +95,7 @@ QSharedPointer<Datafile> loadTiffDat(rcstr filePath) THROWS {
         }
     }
 
-    return datafile;
+    return ret;
 }
 
 #define IS_ASCII RUNTIME_CHECK(2 == dataType, BAD_FORMAT)
@@ -108,7 +108,7 @@ QSharedPointer<Datafile> loadTiffDat(rcstr filePath) THROWS {
     RUNTIME_CHECK(val == dataOffset, BAD_FORMAT)
 
 static void
-loadTiff(QSharedPointer<Datafile>& file, rcstr filePath, deg phi, qreal monitor, qreal expTime) THROWS {
+loadTiff(Datafile* file, rcstr filePath, deg phi, qreal monitor, qreal expTime) THROWS {
 
     Metadata md;
     md.motorPhi = phi;

gui/popup/filedialog.cpp
@@ -60,14 +60,7 @@ QVariant OpenFileProxyModel::data(rcidx idx, int role) const {
                 auto it = memInfo.find(path);
                 if (memInfo.end() != it)
                     return *it;
-                str loadInfo;
-                if (io::couldBeCaress(info))
-                    loadInfo = "[car] " + io::loadCaressComment(path);
-                else if (io::couldBeMar(info))
-                    loadInfo = "[mar] ";
-                else if (io::couldBeTiffDat(info))
-                    loadInfo = "[tif] ";
-
+                str loadInfo = io::loadComment(info);
                 memInfo.insert(path, loadInfo);
                 return loadInfo;
             }

gui/thehub.cpp
@@ -335,7 +335,7 @@ void TheHub::addGivenFile(rcstr filePath) THROWS {
     if (!filePath.isEmpty() && !gSession->hasFile(filePath)) {
         {
             TakesLongTime __;
-            gSession->addGivenFile(io::load(filePath));
+            gSession->addGivenFile(io::loadDatafile(filePath));
         }
         emit sigFilesChanged();
     }
@@ -364,7 +364,7 @@ void TheHub::combineDatasetsBy(pint by) {
 void TheHub::setCorrFile(rcstr filePath) THROWS {
     QSharedPointer<Datafile const> file;
     if (!filePath.isEmpty())
-        file = io::load(filePath);
+        file = io::loadDatafile(filePath);
 
     gSession->setCorrFile(file);
     emit sigCorrFile(file);

utest/test010_io.cpp
@@ -8,7 +8,7 @@ class Session* gSession = Session::instance();
 
 TEST(IO, Caress) {
     try {
-        io::load(TESTDATADIR "/caress.dat");
+        io::loadDatafile(TESTDATADIR "/caress.dat");
     } catch (Exception& ex) {
         std::cerr << "io::load throws: " << ex.what() << "\n";
         EXPECT_TRUE(false);

commit ccae33f53f0f4a609343d079c40bdd8c6a3a47ec

fix bug introduced in 75b607f0 that prevented loading sessions

core/io/io_io.cpp
@@ -113,7 +113,7 @@ str loadComment(QFileInfo const& info) {
     else if (couldBeTiffDat(info))
         return "[tif] ";
     else
-        NEVER;
+        return "";
 }
 
 } // namespace io

core/io/io_io.h
@@ -27,7 +27,6 @@ namespace io {
 //! load a file; file type will be sensed
 QSharedPointer<Datafile const> loadDatafile(rcstr filePath) THROWS;
 
-str loadCaressComment(rcstr filePath);
 str loadComment(QFileInfo const& info);
 
 } // namespace io