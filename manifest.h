8706a315a1670f2d0a97aca30bed020f01025e89
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-01-04 11:44:07 +0100

    link changed to steca; conf location changed to Steca

--- manifest.h
+++ manifest.h
@@ -4,11 +4,11 @@
 #define APPLICATION_NAME "Steca"
 #define APPLICATION_CLAIM "The stress and texture calculator"
 
-#define ORGANIZATION_NAME "Scientific Computing at MLZ"
-#define ORGANIZATION_DOMAIN "apps.jcns.fz-juelich.de"
+#define ORGANIZATION_NAME "Steca"
+#define ORGANIZATION_DOMAIN "steca.mlz-garching.de"
 
 #define STECA2_PAGES_URL "http://apps.jcns.fz-juelich.de/steca"
-#define STECA2_VERSION_URL "http://apps.jcns.fz-juelich.de/src/STeCa2/version.php"
-#define STECA2_DOWNLOAD_URL "https://github.com/scgmlz/STeCa2/releases"
+#define STECA2_VERSION_URL "http://apps.jcns.fz-juelich.de/src/steca/version.php"
+#define STECA2_DOWNLOAD_URL "https://github.com/scgmlz/steca2/releases"
 
 #endif
