bf837b82b913c55bcdfe38807775a50ae8c7d663
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2018-01-05 21:15:06 +0100

    ..

--- gui/widgets/tree_views.h
+++ gui/widgets/tree_views.h
@@ -3,7 +3,7 @@
 //  Steca: stress and texture calculator
 //
 //! @file      gui/widgets/tree_views.h
-//! @brief     Defines classes ListView, MultiListView
+//! @brief     Defines classes TreeView, ListView, MultiListView
 //!
 //! @homepage  https://github.com/scgmlz/Steca
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -20,38 +20,29 @@
 
 class TableModel;
 
-
-//! abstract tree widget
-
+//! Abstract tree widget, base class of ListView and Table
 class TreeView : public QTreeView {
 public:
     TreeView();
 
-    int sizeHintForColumn(int) const; // make narrow columns
+    int sizeHintForColumn(int) const; //!< make narrow columns
 };
 
-//! A (tree-)list view with a reference to the hub. Single selection.
-
+//! A (tree-)list view. Single selection.
 class ListView : public TreeView {
 public:
     ListView() {}
-
     void setModel(TableModel* model);
-
 protected:
     TableModel* model() const;
-
     void updateSingleSelection();
     void selectRow(int);
 };
 
-
-//! A (tree-)list view with a reference to the hub. Multiple selection.
-
+//! A (tree-)list view. Multiple selection.
 class MultiListView : public ListView {
 public:
     MultiListView();
-
 protected:
     void selectRows(uint_vec);
 };
