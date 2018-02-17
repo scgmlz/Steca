33025d4a156f5192c75d6347c5b562e3a0f71f2c
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2018-02-17 22:06:52 +0100

    more tests

--- utest/test021_bounds.cpp
+++ utest/test021_bounds.cpp
@@ -19,12 +19,53 @@ TEST(Bounds, LowerBound) {
         EXPECT_EQ(1, lowerBound(v, +e, 0, n));
         EXPECT_EQ(1, lowerBound(v, 1.-e, 0, n));
         EXPECT_EQ(1, lowerBound(v, 1., 0, n));
+        EXPECT_EQ(n-1, lowerBound(v, n-2+e, 0, n));
         EXPECT_EQ(n-1, lowerBound(v, n-1-e, 0, n));
         EXPECT_EQ(n-1, lowerBound(v, n-1, 0, n));
         EXPECT_EQ(n-1, lowerBound(v, n-1+e, 0, n));
         EXPECT_EQ(n-1, lowerBound(v, n, 0, n));
         EXPECT_EQ(n-1, lowerBound(v, 1e99, 0, n));
     }
+}
+
+TEST(Bounds, LowerBoundArg3) {
+    const int n = 8;
+    vec<deg> v(n);
+    for (int i=0; i<n; ++i)
+        v[i] = i;
+    EXPECT_EQ(3, lowerBound(v, NAN, 3, n));
+    EXPECT_EQ(3, lowerBound(v, -1e99, 3, n));
+    EXPECT_EQ(3, lowerBound(v, -e, 3, n));
+    EXPECT_EQ(3, lowerBound(v, 0., 3, n));
+    EXPECT_EQ(3, lowerBound(v, +e, 3, n));
+    EXPECT_EQ(3, lowerBound(v, 1.-e, 3, n));
+    EXPECT_EQ(3, lowerBound(v, 1., 3, n));
+    EXPECT_EQ(n-1, lowerBound(v, n-2+e, 3, n));
+    EXPECT_EQ(n-1, lowerBound(v, n-1-e, 3, n));
+    EXPECT_EQ(n-1, lowerBound(v, n-1, 3, n));
+    EXPECT_EQ(n-1, lowerBound(v, n-1+e, 3, n));
+    EXPECT_EQ(n-1, lowerBound(v, n, 3, n));
+    EXPECT_EQ(n-1, lowerBound(v, 1e99, 3, n));
+}
+
+TEST(Bounds, LowerBoundArg4) {
+    const int n = 8;
+    vec<deg> v(n);
+    for (int i=0; i<n; ++i)
+        v[i] = i;
+    EXPECT_EQ(0, lowerBound(v, NAN, 0, 4));
+    EXPECT_EQ(0, lowerBound(v, -1e99, 0, 4));
+    EXPECT_EQ(0, lowerBound(v, -e, 0, 4));
+    EXPECT_EQ(0, lowerBound(v, 0., 0, 4));
+    EXPECT_EQ(1, lowerBound(v, +e, 0, 4));
+    EXPECT_EQ(1, lowerBound(v, 1.-e, 0, 4));
+    EXPECT_EQ(1, lowerBound(v, 1., 0, 4));
+    EXPECT_EQ(3, lowerBound(v, n-2+e, 0, 4));
+    EXPECT_EQ(3, lowerBound(v, n-1-e, 0, 4));
+    EXPECT_EQ(3, lowerBound(v, n-1, 0, 4));
+    EXPECT_EQ(3, lowerBound(v, n-1+e, 0, 4));
+    EXPECT_EQ(3, lowerBound(v, n, 0, 4));
+    EXPECT_EQ(3, lowerBound(v, 1e99, 0, 4));
 }
 
 TEST(Bounds, UpperBound) {
