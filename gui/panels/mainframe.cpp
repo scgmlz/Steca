da41a3bc8a984766642bf3d998971f9c0e864dc8
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-03-26 11:02:53 +0200

    auto test diagram

--- gui/panels/mainframe.cpp
+++ gui/panels/mainframe.cpp
@@ -24,7 +24,7 @@
 // ************************************************************************** //
 
 Mainframe::Mainframe()
-    : CTabWidget {"images"}
+    : CTabWidget {"mainframe"}
 {
     // local connection
     connect(this, &Mainframe::currentChanged, [this](int) { render(); });
