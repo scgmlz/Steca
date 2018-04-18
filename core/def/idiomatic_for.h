3cffa6a723a68049e9d6b7d340a0fa45fb9c888a
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-04-18 11:57:30 +0200

    simplify idiomatic_for, and rm unused down loop

--- core/def/idiomatic_for.h
+++ core/def/idiomatic_for.h
@@ -3,7 +3,7 @@
 //  Steca: stress and texture calculator
 //
 //! @file      core/def/idiomatic_for.h
-//! @brief     Defines idiomatic loops (for_int, ...) and ONLY_ONCE
+//! @brief     Defines idiomatic loops (for_int, ...)
 //!
 //! @homepage  https://github.com/scgmlz/Steca
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -15,25 +15,10 @@
 #ifndef IDIOMATIC_FOR_H
 #define IDIOMATIC_FOR_H
 
-#include <QtGlobal> // to define Q_OS_WIN
-
-#ifdef Q_OS_WIN
-
-// MSVC does not handle decltype etc. well, and we disregard int/int warnings on Windows, anyway
-#define for_int(i, n) for (int i = 0, i##End = (n); i < i##End; ++i)
-#define for_int_down(n) for (int i = (num); i-- > 0;)
-
-#else
-
 #define for_int(i, n) \
-    for (std::remove_const<decltype(n)>::type i = decltype(n)(0), i##End = (n); i < i##End; ++i)
-#define for_int_down(n) for (std::remove_const<decltype(n)>::type i = (num); i-- > decltype(n)(0);)
-
-#endif
+    for (int i = 0, i##End = (n); i < i##End; ++i)
 
 #define for_i(n) for_int (i, n)
-
-#define for_i_down(n) for_int_down (i, n)
 
 #define for_ij(ni, nj)  \
     for_int (i, ni)     \
