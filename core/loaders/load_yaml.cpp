07237fbe52fe2fc4af07008d90ce2816b124199f
Joachim Coenen <joachimcoenen@icloud.com>
2018-04-24 11:02:18 +0200

    cleanup in yaml_loader.cpp

--- core/loaders/load_yaml.cpp
+++ core/loaders/load_yaml.cpp
@@ -12,117 +12,54 @@
 //
 //  ***********************************************************************************************
 
-#include "3rdparty/yaml-cpp/include/yaml-cpp/yaml.h" // inclusion order is critical ?
-
-//#include "core/def/idiomatic_for.h"
 #include "core/raw/rawfile.h"
-//#include <qmath.h>
-
-
-; // preserve inclusion order
-
-namespace  {
-void readInstrument(const YAML::Node& node, Rawfile& rawfile);
-void readFormat(const YAML::Node& node, Rawfile& rawfile);
-void readExperiment(const YAML::Node& node, Rawfile& rawfile);
-void readMeasurement(const YAML::Node& node, Rawfile& rawfile);
-
-typedef std::function<void (const YAML::Node& node, Rawfile& rawfile)> ParserFunction;
-
-const std::map<std::string, ParserFunction> parserFunctions = {
-    std::pair<std::string, ParserFunction>("instrument",  &readInstrument),
-    std::pair<std::string, ParserFunction>("format",      &readFormat),
-    std::pair<std::string, ParserFunction>("experiment",  &readExperiment),
-    std::pair<std::string, ParserFunction>("measurement", &readMeasurement),
-};
-
-}
-
-
-namespace load {
-
-
-
-Rawfile loadYaml(const QString& filePath)
-{
-    YAML::Node yamlFile = YAML::LoadFile(filePath.toStdString()); // throws: ParserException, BadFile;
-
-    Rawfile rawfile(filePath);
-    for (auto keywordHandler : parserFunctions ) {
-        const auto keyword = keywordHandler.first;
-        const auto func = keywordHandler.second;
-        func(yamlFile[keyword], rawfile);
-    }
-
-    return rawfile;
-}
-
-} // namespace load
-
+#include "3rdparty/yaml-cpp/include/yaml-cpp/yaml.h"
 
 namespace  {
 
 void readInstrument(const YAML::Node& node, Rawfile& rawfile)
 {
-    if (!node.IsDefined()) {
+    if (!node.IsDefined())
         return;
-    };
 
     const auto name       = node["name"].as<std::string>("");
     const auto operators  = node["operators"].as<std::vector<std::string>>();
     const auto facility   = node["facility"].as<std::string>("");
     const auto website    = node["website"].as<std::string>("");
     const auto references = node["references"].as<std::vector<std::string>>();
-    // TODO: readInstrument(const YAML::Node& node, const Rawfile& rawfile)
 }
 
 void readFormat(const YAML::Node& node, Rawfile& rawfile)
 {
-    if (!node.IsDefined()) {
+    if (!node.IsDefined())
         return;
-    };
 
     const auto identifier = node["identifier"].as<std::string>("");
     // units = name: unit. eg: time: second, or clearance: millimeter:
     const auto units  = node["units"].as<std::map<std::string, std::string>>();
-    // TODO: readFormat(const YAML::Node& node, const Rawfile& rawfile)
 }
 
 void readExperiment(const YAML::Node& node, Rawfile& rawfile)
 {
-    if (!node.IsDefined()) {
+    if (!node.IsDefined())
         return;
-    };
 
-    const auto number = node["number"].as<std::string>(""); // Integer maybe?
-    const auto proposal = node["proposal"].as<std::string>(""); // not an Integer!!
+    const auto number = node["number"].as<std::string>("");
+    const auto proposal = node["proposal"].as<std::string>("");
     const auto title = node["title"].as<std::string>("");
-    // const auto authors = node["authors"].as<std::vector<Author>>(""); TODO: uncomment.
     const auto remark = node["remark"].as<std::string>("");
-
-    /*
-    author:
-
-    name: "Dr. John Doe"
-    email: John.Doe@frm2.tum.de
-    roles:
-    -   local_contact
-    -   principal_investigator
-    */
-    // TODO: readExperiment(const YAML::Node& node, const Rawfile& rawfile)
 }
 
 void readSample(const YAML::Node& node, Metadata& metadata)
 {
     metadata.comment = QString::fromStdString(node["description"]["name"].as<std::string>());
-    metadata.motorXT = node["position"]["xt"]["value"].as<double>(NAN);//double
+    metadata.motorXT = node["position"]["xt"]["value"].as<double>(NAN);
     metadata.motorYT = node["position"]["yt"]["value"].as<double>(NAN);
     metadata.motorZT = node["position"]["zt"]["value"].as<double>(NAN);
     metadata.motorOmg = node["orientation"]["omgs"]["value"].as<double>(NAN);
     metadata.motorTth = node["orientation"]["tths"]["value"].as<double>(NAN);
     metadata.motorPhi = node["orientation"]["phis"]["value"].as<double>(NAN);
     metadata.motorChi = node["orientation"]["chis"]["value"].as<double>(NAN);
-    // TODO: readSample(const YAML::Node& node, const Metadata& metadata)
 }
 
 void readSetup(const YAML::Node& node, Metadata& metadata)
@@ -130,7 +67,6 @@ void readSetup(const YAML::Node& node, Metadata& metadata)
     metadata.motorPST = NAN; // node["orientation"]["xt"]["value"].as<double>(NAN);
     metadata.motorSST = NAN; // node["orientation"]["xt"]["value"].as<double>(NAN);
     metadata.motorOMGM = node["monochromator"]["omgm"]["value"].as<double>(NAN);
-    // type = double:
     metadata.nmT;
     metadata.nmTeload;
     metadata.nmTepos;
@@ -143,11 +79,9 @@ void readSetup(const YAML::Node& node, Metadata& metadata)
 
 void readSingleScan(const YAML::Node& node, Metadata& metadata, Rawfile& rawfile)
 {
-    if (!node.IsDefined()) {
+    if (!node.IsDefined())
         return;
-    };
 
-    // const auto scan_parameters = node["scan_parameters"].as<std::tuple>();
     metadata.time = node["time"].as<double>(NAN);
     metadata.monitorCount = node["monitor"].as<double>(NAN);
     const auto sum = node["sum"].as<double>(NAN);
@@ -167,20 +101,16 @@ void readSingleScan(const YAML::Node& node, Metadata& metadata, Rawfile& rawfile
 
 void readScans(const YAML::Node& node, Metadata& metadata, Rawfile& rawfile)
 {
-    if (!node.IsDefined()) {
+    if (!node.IsDefined())
         return;
-    };
-    if (!node.IsSequence()) {
+    if (!node.IsSequence())
         THROW("invalid YAML format: 'scans' should be a list, but isn't.")
-    };
 
     for (const YAML::Node& innerNode: node) {
-        Metadata metadataCopy;
+        Metadata metadataCopy(std::move(metadata));
         { // Copy the metadata in a dirty way:
-            // type = string:
             metadataCopy.date              =  metadata.date             ;
             metadataCopy.comment           =  metadata.comment          ;
-            // type = deg:
             metadataCopy.motorXT           =  metadata.motorXT          ;
             metadataCopy.motorYT           =  metadata.motorYT          ;
             metadataCopy.motorZT           =  metadata.motorZT          ;
@@ -191,7 +121,6 @@ void readScans(const YAML::Node& node, Metadata& metadata, Rawfile& rawfile)
             metadataCopy.motorPST          =  metadata.motorPST         ;
             metadataCopy.motorSST          =  metadata.motorSST         ;
             metadataCopy.motorOMGM         =  metadata.motorOMGM        ;
-            // type = double:
             metadataCopy.nmT               =  metadata.nmT              ;
             metadataCopy.nmTeload          =  metadata.nmTeload         ;
             metadataCopy.nmTepos           =  metadata.nmTepos          ;
@@ -214,24 +143,15 @@ void readScans(const YAML::Node& node, Metadata& metadata, Rawfile& rawfile)
 
 void readMeasurement(const YAML::Node& node, Rawfile& rawfile)
 {
-    if (!node.IsDefined()) {
+    if (!node.IsDefined())
         return;
-    };
 
     const auto unique_identifier = node["unique_identifier"].as<std::string>("");
     const auto number = node["number"].as<std::string>(""); // Integer maybe?
 
     auto metadata = Metadata();
 
-    // type = string:
     metadata.date = QString::fromStdString(node["history"]["started"].as<std::string>());
-    // type = deg:
-
-    metadata.monitorCount;
-    metadata.deltaMonitorCount;
-
-    metadata.time;
-    metadata.deltaTime;
 
     readSample(node["sample"], metadata);
     readSetup(node["setup"], metadata);
@@ -240,5 +160,22 @@ void readMeasurement(const YAML::Node& node, Rawfile& rawfile)
     // TODO: readMeasurement(const YAML::Node& node, const Rawfile& rawfile)
 }
 
-
 } // namespace
+
+
+namespace load {
+
+Rawfile loadYaml(const QString& filePath)
+{
+    YAML::Node yamlFile = YAML::LoadFile(filePath.toStdString()); // throws: ParserException, BadFile;
+
+    Rawfile rawfile(filePath);
+    readInstrument (yamlFile["instrument"] , rawfile);
+    readFormat     (yamlFile["format"]     , rawfile);
+    readExperiment (yamlFile["experiment"] , rawfile);
+    readMeasurement(yamlFile["measurement"], rawfile);
+
+    return rawfile;
+}
+
+} // namespace load
