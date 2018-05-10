1088942e6092f474a6de2c307825045c016c4ab7
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2018-05-10 22:46:56 +0200

    cell update implemented, but untested

--- qcr/engine/cell.cpp
+++ qcr/engine/cell.cpp
@@ -13,5 +13,31 @@
 //  ***********************************************************************************************
 
 #include "cell.h"
+#include <QtDebug>
 
-FinalCell::latestTimestamp__ = 0;
+Cell::stamp_t FinalCell::latestTimestamp__ = 0;
+
+void Cell::add_source(Cell* src) {
+    if (sources_.find(src)!=sources_.end())
+        qFatal("duplicate cell source entry");
+    sources_.insert(src);
+}
+
+void Cell::rm_source(Cell* src) {
+    auto it = sources_.find(src);
+    if (it==sources_.end())
+        qFatal("failed removing cell source entry");
+    sources_.erase(it);
+}
+
+Cell::stamp_t Cell::update()
+{
+    stamp_t t_new = timestamp_;
+    for (Cell* src: sources_)
+        t_new = std::max(t_new, src->update());
+    if (t_new>timestamp_) {
+        recompute();
+        timestamp_ = t_new;
+    }
+    return timestamp_;
+}
--- qcr/engine/cell.h
+++ qcr/engine/cell.h
@@ -15,7 +15,7 @@
 #ifndef CELL_H
 #define CELL_H
 
-#include <list>
+#include <set>
 
 //! Manages update dependences.
 class Cell
@@ -23,15 +23,21 @@ class Cell
 public:
     Cell() {}
     typedef long int stamp_t;
+    stamp_t update();
+    void add_source(Cell*);
+    void rm_source(Cell*);
 protected:
+    virtual void recompute() = 0;
 private:
     stamp_t timestamp_ { 0 };
+    std::set<Cell*> sources_;
 };
 
 class FinalCell : public Cell {
 private:
     static stamp_t latestTimestamp__;
     static stamp_t mintTimestamp() { return ++latestTimestamp__; }
+    void recompute() final {};
 };
 
 #endif // CELL_H
