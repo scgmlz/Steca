a9997b1252d718704bf00fbcb6248f07b3713261
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2018-05-10 22:09:25 +0200

    cell.cpp was missing

---
+++ qcr/engine/cell.cpp
@@ -0,0 +1,17 @@
+//  ***********************************************************************************************
+//
+//  libqcr: capture and replay Qt widget actions
+//
+//! @file      qcr/engine/cell.cpp
+//! @brief     Implements class Cell
+//!
+//! @homepage  https://github.com/scgmlz/Steca
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018-
+//! @author    Joachim Wuttke
+//
+//  ***********************************************************************************************
+
+#include "cell.h"
+
+FinalCell::latestTimestamp__ = 0;
