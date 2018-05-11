bf6a9cb3ae5431b7d812b48ccf07ed0f45ca70dd
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-05-11 14:23:24 +0200

    split definition and implementation

--- qcr/engine/single_value.h
+++ qcr/engine/single_value.h
@@ -3,7 +3,7 @@
 //  libqcr: capture and replay Qt widget actions
 //
 //! @file      qcr/engine/single_value.h
-//! @brief     Defines templated classes QcrControl
+//! @brief     Defines and implements templated classes QcrControl
 //!
 //! @homepage  https://github.com/scgmlz/Steca
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -22,38 +22,61 @@
 template<class T>
 class QcrControl : protected CSettable {
 public:
-    QcrControl(const QString& name)
-        : CSettable(name) {}
-    void programaticallySetValue(T val) {
-        softwareCalling_ = true;
-        doSetValue(val);
-        softwareCalling_ = false;
-    }
+    QcrControl(const QString& name);
+    void programaticallySetValue(T val);
     virtual T getValue() const = 0;
-    virtual void onCommand(const QString& arg) {
-        programaticallySetValue(strOp::from_s<T>(arg));
-    }
+    virtual void onCommand(const QString& arg);
 protected:
-    void init() {
-        reportedValue_ = getValue();
-        doLog(true, "initialization: "+name()+" "+strOp::to_s(reportedValue_));
-    }
-    void onChangedValue(bool hasFocus, T val) {
-        if (val==reportedValue_)
-            return; // nothing to do
-        bool userCall = hasFocus && !softwareCalling_;
-        doLog(!userCall, name()+" "+strOp::to_s(val));
-        if (hasFocus && softwareCalling_)
-            qDebug() << "UNEXPECTED in "+name()+" hasFocus && softwareCalling_";
-        if (!hasFocus && !softwareCalling_)
-            qDebug() << "UNDESIRABLE in "+name()+" !hasFocus && !softwareCalling_";
-        // not sure whether we want to get rid of hasFocus
-        reportedValue_ = val;
-    }
+    void init();
+    void onChangedValue(bool hasFocus, T val);
     bool softwareCalling_ = false; // make it private again ??
 private:
     virtual void doSetValue(T) = 0;
     T reportedValue_;
 };
+
+//  ***********************************************************************************************
+//  implementation of QcrControl<T>
+
+template<class T>
+QcrControl<T>::QcrControl(const QString& name)
+    : CSettable(name)
+{}
+
+template<class T>
+void QcrControl<T>::programaticallySetValue(T val)
+{
+    softwareCalling_ = true;
+    doSetValue(val);
+    softwareCalling_ = false;
+}
+
+template<class T>
+void QcrControl<T>::onCommand(const QString& arg)
+{
+    programaticallySetValue(strOp::from_s<T>(arg));
+}
+
+template<class T>
+void QcrControl<T>::init()
+{
+    reportedValue_ = getValue();
+    doLog(true, "initialization: "+name()+" "+strOp::to_s(reportedValue_));
+}
+
+template<class T>
+void QcrControl<T>::onChangedValue(bool hasFocus, T val)
+{
+    if (val==reportedValue_)
+        return; // nothing to do
+    bool userCall = hasFocus && !softwareCalling_;
+    doLog(!userCall, name()+" "+strOp::to_s(val));
+    if (hasFocus && softwareCalling_)
+        qDebug() << "UNEXPECTED in "+name()+" hasFocus && softwareCalling_";
+    if (!hasFocus && !softwareCalling_)
+        qDebug() << "UNDESIRABLE in "+name()+" !hasFocus && !softwareCalling_";
+    // not sure whether we want to get rid of hasFocus
+    reportedValue_ = val;
+}
 
 #endif // SINGLE_VALUE_H
