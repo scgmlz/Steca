ad437be19cc851dbbab170624cb17d4d3117bc74
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2018-07-13 09:12:31 +0200

    rm unused NumberWrapper's min/max mechanism

--- qcr/engine/param_wrapper.h
+++ qcr/engine/param_wrapper.h
@@ -50,20 +50,7 @@ protected:
 template<class T>
 class NumberWrapper : public ParamWrapper<T> {
 public:
-    NumberWrapper(T value) : ParamWrapper<T>{value} {
-        ParamWrapper<T>::coerce_ = [this](T val) { return coerceMinMax(val); }; }
-    // NumberWrapper(T value, T _min, T _max) : NumberWrapper<T>{value} { setMin(_min); setMax(_max); }
-    void setMinFct(const std::function<T()>& f) { minFct_ = f; hasMin_ = true; }
-    void setMaxFct(const std::function<T()>& f) { maxFct_ = f; hasMax_ = true;  }
-    void setMin(const T mval) { setMinFct( [mval](){ return mval; } ); }
-    void setMax(const T mval) { setMaxFct( [mval](){ return mval; } ); }
-
-private:
-    bool hasMin_ {false};
-    bool hasMax_ {false};
-    const std::function<T()> minFct_ {[]() { return T(); }};
-    const std::function<T()> maxFct_ {[]() { return T(); }};
-    T coerceMinMax(T val) const;
+    NumberWrapper(T value) : ParamWrapper<T>{value} {}
 };
 
 //  ***********************************************************************************************
@@ -96,19 +83,6 @@ void ParamWrapper<T>::guiSetsVal(T val, bool userCall)
     } else {
         ;// qDebug() << " -> " << val << " (non-user call)";
     }
-}
-
-//  ***********************************************************************************************
-//  class NumberWrapper implementation
-
-template<class T>
-T NumberWrapper<T>::coerceMinMax(T val) const
-{
-    if (hasMin_)
-        val = qMax(val, minFct_());
-    if (hasMax_)
-        val = qMin(val, maxFct_());
-    return val;
 }
 
 #endif // PARAM_WRAPPER_H
