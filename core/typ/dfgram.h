b5fc08e719ab53adb94952b064c2e0bf417e817a
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-07-30 17:34:32 +0200

    start dfgram.h

---
+++ core/typ/dfgram.h
@@ -0,0 +1,29 @@
+//  ***********************************************************************************************
+//
+//  Steca: stress and texture calculator
+//
+//! @file      core/typ/dfgram.h
+//! @brief     Defines class Dfgram
+//!
+//! @homepage  https://github.com/scgmlz/Steca
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2016-2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, MAINTAINER)
+//
+//  ***********************************************************************************************
+
+#ifndef DFGRAM_H
+#define DFGRAM_H
+
+#include "core/typ/curve.h"
+
+//! A curve, and associated fit parameters
+
+class Dfgram {
+public:
+    Dfgram(Curve&& c) : curve(std::move(c)) {}
+    Curve curve;
+private:
+};
+
+#endif // DFGRAM_H
