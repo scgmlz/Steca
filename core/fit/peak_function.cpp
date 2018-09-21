bdebb198ebfbd66f9a44efe246c926cab66aaef7
Joachim Coenen <joachimcoenen@icloud.com>
2018-09-21 11:39:38 +0200

    cleanup
    Re #177

--- core/fit/peak_function.cpp
+++ core/fit/peak_function.cpp
@@ -146,40 +146,10 @@ void Voigt::setY(const double *P, const int nXY, const double *X, double *Y) con
             //const DoubleWithError intensity;
 }
 
-double pow1(double x) { return x; }
-double pow2(double x) { return x*x; }
-double pow3(double x) { return x*x*x; }
-double pow4(double x) { return x*x*x*x; }
-
 void Voigt::setDY(const double* P, const int nXY, const double* X, double* Jacobian) const
 {
-    // V[x - a, b, c] * f
-//    funct == 1
-//            * 1/(2 c Sqrt[2 \[Pi]])
-//            * f (E^((b - I (-a + x))^2/(2 c^2)) Erfc[(b - I (-a + x))/(Sqrt[2] c)] + E^((b + I (-a + x))^2/(2 c^2)) Erfc[(b + I (-a + x))/(Sqrt[2] c)])
 
     for (int i=0; i<nXY; ++i) {
-        *Jacobian++ = voigt(X[i] - (P[0] + 0.5), P[1], P[2]) * P[3] - voigt(X[i] - (P[0] - 0.5), P[1], P[2]) * P[3];
-        *Jacobian++ = voigt(X[i] - P[0], P[1] + 0.5, P[2]) * P[3]   - voigt(X[i] - P[0], P[1] - 0.5, P[2]) * P[3];
-        *Jacobian++ = voigt(X[i] - P[0], P[1], P[2] + 0.5) * P[3]   - voigt(X[i] - P[0], P[1], P[2] - 0.5) * P[3];
-        *Jacobian++ = voigt(X[i] - P[0], P[1], P[2]) * (P[3] + 0.5) - voigt(X[i] - P[0], P[1], P[2]) * (P[3] - 0.5);
-//        P[0] == (1/(2 c Sqrt[2 \[Pi]]))f ((I E^((b - I (-a + x))^2/(2 c^2)) (b - I (-a + x)) Erfc[(b - I (-a + x))/(Sqrt[2] c)])/c^2 - (I E^((b + I (-a + x))^2/(2 c^2)) (b + I (-a + x)) Erfc[(b + I (-a + x))/(Sqrt[2] c)])/c^2);
-//                 1/(2 c Sqrt[2 \[Pi]]) f (E^((b - I (-a + x))^2/(2 c^2)) Erfc[(b - I (-a + x))/(Sqrt[2] c)] + E^((b + I (-a + x))^2/(2 c^2)) Erfc[(b + I (-a + x))/(Sqrt[2] c)])
-//        P[0] == (1/(2 c Sqrt[2 \[Pi]]))f ((I E^((b - I (-a + x))^2/(2 c^2)) (b - I (-a + x)) Erfc[(b - I (-a + x))/(Sqrt[2] c)]) - (I E^((b + I (-a + x))^2/(2 c^2)) (b + I (-a + x)) Erfc[(b + I (-a + x))/(Sqrt[2] c)]))/c^2;
-
-
-//        P[0] == 1
-//                * 1/(2*c^3 Sqrt[2 \[Pi]])
-//                * I
-//                * f
-//                * (E^(-((-a + I b + x)^2/(2 c^2))) (I a + b - I x) Erfc[(I a + b - I x)/(Sqrt[2] c)] - E^((b + I (-a + x))^2/(2 c^2)) (b + I (-a + x)) Erfc[(b + I (-a + x))/(Sqrt[2] c)])
-
-//        P[1] == (1/(2 c^3 Sqrt[2 \[Pi]]))f (-2 c Sqrt[2/\[Pi]] + E^(-((-a + I b + x)^2/(2 c^2))) (I a + b - I x) Erfc[(I a + b - I x)/(Sqrt[2] c)] + E^((b + I (-a + x))^2/(2 c^2)) (b + I (-a + x)) Erfc[(b + I (-a + x))/(Sqrt[2] c)])
-
-//        P[2] == -(1/(2 c^4 Sqrt[2 \[Pi]]))f (-2 b c Sqrt[2/\[Pi]] + c^2 E^(-((-a + I b + x)^2/(2 c^2))) Erfc[(I a + b - I x)/(Sqrt[2] c)] - E^(-((-a + I b + x)^2/(2 c^2))) (-a + I b + x)^2 Erfc[(I a + b - I x)/(Sqrt[2] c)] + c^2 E^((b + I (-a + x))^2/(2 c^2)) Erfc[(b + I (-a + x))/(Sqrt[2] c)] + E^((b + I (-a + x))^2/(2 c^2)) (b + I (-a + x))^2 Erfc[(b + I (-a + x))/(Sqrt[2] c)])
-
-//        P[3] == (E^(-((a + I b - x)^2/(2 c^2))) (E^((2 I b (a - x))/c^2) Erfc[(I a + b - I x)/(Sqrt[2] c)] + Erfc[(b + I (-a + x))/(Sqrt[2] c)]))/(2 c Sqrt[2 \[Pi]])
-
     }
 }
 
