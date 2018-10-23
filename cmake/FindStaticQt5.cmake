9c6ad64b0ec94a5dd52a98e2aac4ae26e379eec3
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-10-23 19:23:37 +0200

    clean up, prepare for adaptation to Win

--- cmake/FindStaticQt5.cmake
+++ cmake/FindStaticQt5.cmake
@@ -1,24 +1,24 @@
 ## parametrization (version, platform-specific paths)
 
-set(qt5_version 5.11.1)
-set(_qt5_install_prefix /usr)
-set(_qt5_include_prefix ${_qt5_install_prefix}/include/x86_64-linux-gnu/qt5)
-set(lib_path ${_qt5_install_prefix}/lib/x86_64-linux-gnu)
+set(qt5_lib_extension so.5.11.1)
+set(qt5_install_prefix /usr)
+set(qt5_include_prefix ${qt5_install_prefix}/include/x86_64-linux-gnu/qt5)
+set(qt5_lib_path ${qt5_install_prefix}/lib/x86_64-linux-gnu)
 
 ## set paths
 
 set(qt5_components Core Gui Widgets Network)
 foreach(comp ${qt5_components})
-    set(${comp}_path "${lib_path}/libQt5${comp}.so.${qt5_version}")
+    set(${comp}_path "${qt5_lib_path}/libQt5${comp}.${qt5_lib_extension}")
     set(Qt5${comp}_INCLUDE_DIRS
-        ${_qt5_include_prefix}
-        ${_qt5_include_prefix}/Qt${comp})
+        ${qt5_include_prefix}
+        ${qt5_include_prefix}/Qt${comp})
 endforeach()
 
-list(APPEND Qt5Gui_INCLUDE_DIRS ${_qt5_include_prefix}/QtCore)
-list(APPEND Qt5Widgets_INCLUDE_DIRS ${_qt5_include_prefix}/QtCore)
-list(APPEND Qt5Widgets_INCLUDE_DIRS ${_qt5_include_prefix}/QtGui)
-list(APPEND Qt5Network_INCLUDE_DIRS ${_qt5_include_prefix}/QtCore)
+list(APPEND Qt5Gui_INCLUDE_DIRS     ${qt5_include_prefix}/QtCore)
+list(APPEND Qt5Widgets_INCLUDE_DIRS ${qt5_include_prefix}/QtCore)
+list(APPEND Qt5Widgets_INCLUDE_DIRS ${qt5_include_prefix}/QtGui)
+list(APPEND Qt5Network_INCLUDE_DIRS ${qt5_include_prefix}/QtCore)
 
 set(Gui_LIB_DEPENDENCIES     "Qt5::Core")
 set(Widgets_LIB_DEPENDENCIES "Qt5::Gui;Qt5::Core")
@@ -49,7 +49,6 @@ foreach(comp ${qt5_components})
     set_target_properties(Qt5::${comp} PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES   "${Qt5${comp}_INCLUDE_DIRS}"
         INTERFACE_LINK_LIBRARIES        "${${comp}_LIB_DEPENDENCIES}"
-
         IMPORTED_LOCATION_RELEASE       ${${comp}_path}
         )
     set_property(TARGET Qt5::${comp} APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -63,7 +62,7 @@ foreach(comp Core Gui Widgets)
     macro(_qt5_${comp}_check_file_exists file)
         check_file_exists(${file})
     endmacro()
-    set(_qt5${comp}_install_prefix ${_qt5_install_prefix})
+    set(_qt5${comp}_install_prefix ${qt5_install_prefix})
     include("${Qt5ConfigDir}/Qt5${comp}/Qt5${comp}ConfigExtras.cmake")
 endforeach()
 
