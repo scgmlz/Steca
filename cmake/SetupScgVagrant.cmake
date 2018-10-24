172618abadf70785ca3fe0f0464d5a4c51a21621
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-10-24 09:19:25 +0200

    one more diff between Linux and Windows

--- cmake/SetupScgVagrant.cmake
+++ cmake/SetupScgVagrant.cmake
@@ -1,7 +1,7 @@
 ## parametrization (version, platform-specific paths)
 
 set(qt5_settings "ScgVagrant" CACHE INTERNAL "")
-set(qt5_lib_extension lib CACHE INTERNAL "")
 set(qt5_install_prefix C:/Development/qtbase-everywhere-src-5.11.2 CACHE INTERNAL "")
 set(qt5_include_prefix ${qt5_install_prefix}/include CACHE INTERNAL "")
-set(qt5_lib_path ${qt5_install_prefix}/lib CACHE INTERNAL "")
+set(qt5_lib_prefix ${qt5_install_prefix}/lib/ CACHE INTERNAL "")
+set(qt5_lib_postfix lib CACHE INTERNAL "")
