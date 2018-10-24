ab7fc8bb886fa50aaa90f36452e0e7ec6a715811
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-10-24 09:02:34 +0200

    paths for Vagrant

--- cmake/SetupScgVagrant.cmake
+++ cmake/SetupScgVagrant.cmake
@@ -1,7 +1,7 @@
 ## parametrization (version, platform-specific paths)
 
-set(qt5_settings "JWuOffice" CACHE INTERNAL "")
-set(qt5_lib_extension so.5.11.1 CACHE INTERNAL "")
-set(qt5_install_prefix /usr CACHE INTERNAL "")
-set(qt5_include_prefix ${qt5_install_prefix}/include/x86_64-linux-gnu/qt5 CACHE INTERNAL "")
-set(qt5_lib_path ${qt5_install_prefix}/lib/x86_64-linux-gnu CACHE INTERNAL "")
+set(qt5_settings "ScgVagrant" CACHE INTERNAL "")
+set(qt5_lib_extension lib CACHE INTERNAL "")
+set(qt5_install_prefix C:\Development\qtbase-everywhere-src-5.11.2 CACHE INTERNAL "")
+set(qt5_include_prefix ${qt5_install_prefix}\include CACHE INTERNAL "")
+set(qt5_lib_path ${qt5_install_prefix}\lib\ CACHE INTERNAL "")
