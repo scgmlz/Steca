54ce23d6a7e263ea6aac4f5c5685b0ac7efc8435
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-11-05 20:27:10 +0100

    Outfactor function

--- cmake/PackIFW.cmake
+++ cmake/PackIFW.cmake
@@ -29,36 +29,44 @@ if(NOT DEFINED ENV{QTDIR})
     message(FATAL_ERROR "QTDIR not defined")
 endif()
 find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS $ENV{QTDIR}/bin)
+mark_as_advanced(WINDEPLOYQT_EXECUTABLE)
 message(STATUS "found windeployqt tool at ${WINDEPLOYQT_EXECUTABLE}")
 if(NOT WINDEPLOYQT_EXECUTABLE)
     message(FATAL_ERROR "windeployqt not found")
 endif()
 
+
+#########################################################################################
+
+cpack_add_component_group(Steca EXPANDED)
+cpack_ifw_configure_component_Group(Steca NAME fzj.jcns.scg.steca)
+
+install(TARGETS Steca DESTINATION bin COMPONENT Steca.exe)
+
 # Add commands that copy the Qt runtime to the target's output directory after
 # build and install the Qt runtime to the specified directory
-function(windeployqt target directory)
 
-  # Run windeployqt immediately after build
-  add_custom_command(TARGET ${target} POST_BUILD
+# Run windeployqt immediately after build
+add_custom_command(TARGET Steca POST_BUILD
     COMMAND "${CMAKE_COMMAND}" -E
     env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
     --verbose 0
     --no-compiler-runtime
-    \"$<TARGET_FILE:${target}>\"
+    \"$<TARGET_FILE:Steca>\"
     )
 
-  # install(CODE ...) doesn't support generator expressions, but
-  # file(GENERATE ...) does - store the path in a file
-  file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${target}_$<CONFIG>"
-    CONTENT "$<TARGET_FILE:${target}>"
+# install(CODE ...) doesn't support generator expressions, but
+# file(GENERATE ...) does - store the path in a file
+file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/Steca_$<CONFIG>"
+    CONTENT "$<TARGET_FILE:Steca>"
     CONDITION "$<CONFIG:Release>"
     )
 
-  # Before installation, run a series of commands that copy each of the Qt
-  # runtime files to the appropriate directory for installation
-  install(CODE
+# Before installation, run a series of commands that copy each of the Qt
+# runtime files to the appropriate directory for installation
+install(CODE
     "
-    file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${target}_Release\" _file)
+    file(READ \"${CMAKE_CURRENT_BINARY_DIR}/Steca_Release\" _file)
     execute_process(
       COMMAND \"${CMAKE_COMMAND}\" -E
       env PATH=\"${_qt_bin_dir}\" \"${WINDEPLOYQT_EXECUTABLE}\"
@@ -75,32 +83,23 @@ function(windeployqt target directory)
       list(GET _files 1 _dest)
       execute_process(
         COMMAND \"${CMAKE_COMMAND}\" -E
-        copy \${_src} \"\${CMAKE_INSTALL_PREFIX}/${directory}/\${_dest}\"
+        copy \${_src} \"\${CMAKE_INSTALL_PREFIX}/bin/\${_dest}\"
         )
       list(REMOVE_AT _files 0 1)
     endwhile()
     "
     COMPONENT "QtLibs"
     )
-  # windeployqt doesn't work correctly with the system runtime libraries,
-  # so we fall back to one of CMake's own modules for copying them over
-  foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+# windeployqt doesn't work correctly with the system runtime libraries,
+# so we fall back to one of CMake's own modules for copying them over
+foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
     get_filename_component(filename "${lib}" NAME)
-    add_custom_command(TARGET ${target} POST_BUILD
-      COMMAND "${CMAKE_COMMAND}" -E
-      copy_if_different "${lib}" \"$<TARGET_FILE_DIR:${target}>\"
-      )
-  endforeach()
-endfunction()
-
-mark_as_advanced(WINDEPLOYQT_EXECUTABLE)
-#########################################################################################
-
-cpack_add_component_group(Steca EXPANDED)
-cpack_ifw_configure_component_Group(Steca NAME fzj.jcns.scg.steca)
-
-install(TARGETS Steca DESTINATION bin COMPONENT Steca.exe)
-windeployqt("Steca" "bin")
+    add_custom_command(TARGET Steca POST_BUILD
+        COMMAND "${CMAKE_COMMAND}" -E
+        copy_if_different "${lib}" \"$<TARGET_FILE_DIR:Steca>\"
+        )
+endforeach()
 
 cpack_add_component(Steca.exe ENABLED GROUP Steca)
-cpack_ifw_configure_component(Steca.exe NAME fzj.jcns.scg.steca.exe SCRIPT ${CMAKE_SOURCE_DIR}/StecaInstallScript.qs)
+cpack_ifw_configure_component(Steca.exe
+    NAME fzj.jcns.scg.steca.exe SCRIPT ${CMAKE_SOURCE_DIR}/StecaInstallScript.qs)
