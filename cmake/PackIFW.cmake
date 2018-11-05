3f6e2b6ca7e743676b4f02f38e0ee44c764e197e
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-11-05 19:09:35 +0100

    Merge cpack modules

--- cmake/PackIFW.cmake
+++ cmake/PackIFW.cmake
@@ -1,3 +1,28 @@
+# Steca Windows packaging #
+
+
+# Name of the package
+set(CPACK_PACKAGE_FILE_NAME
+  "${CMAKE_PROJECT_NAME}-Installer")
+
+# Enable IFW generator
+set(CPACK_GENERATOR IFW)
+
+# Default Directories for installation and Start Menu
+set(CPACK_IFW_PACKAGE_START_MENU_DIRECTORY "A Steca Istallation with IFW")
+set(CPACK_IFW_TARGET_DIRECTORY "@HomeDir@/${CMAKE_PROJECT_NAME}")
+set(CPACK_IFW_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/main/icons/Steca.ico")
+set(CPACK_IFW_PACKAGE_WINDOW_ICON "${CMAKE_SOURCE_DIR}/main/icons/Steca.png")
+
+# If not set, all components will be merged in the group
+set(CPACK_COMPONENTS_GROUPING IGNORE)
+
+# The parent group
+set(CPACK_IFW_PACKAGE_GROUP Steca)
+
+include(CPack)
+include(CPackIFW)
+
 #The following code was adapted from "Using windeployqt with CPack":
 #https://hk.saowen.com/a/d1cf90fcfea6d511629fd5a6c8113808721a7f19656677e8a5fab370a8d35cd4
 #########################################################################################
@@ -41,7 +66,7 @@ function(windeployqt target directory)
   # Before installation, run a series of commands that copy each of the Qt
   # runtime files to the appropriate directory for installation
   install(CODE
-    "    
+    "
     file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${target}_Release\" _file)
     execute_process(
       COMMAND \"${CMAKE_COMMAND}\" -E
@@ -79,3 +104,15 @@ endfunction()
 
 mark_as_advanced(WINDEPLOYQT_EXECUTABLE)
 #########################################################################################
+
+# 1. add/configure component group
+# 2. install component
+# 3. add/configure component
+cpack_add_component_group(Steca EXPANDED)
+cpack_ifw_configure_component_Group(Steca NAME fzj.jcns.scg.steca)
+install(TARGETS Steca
+  DESTINATION bin
+  COMPONENT Steca.exe)
+windeployqt("Steca" "bin")
+cpack_add_component(Steca.exe ENABLED GROUP Steca)
+cpack_ifw_configure_component(Steca.exe NAME fzj.jcns.scg.steca.exe SCRIPT ${CMAKE_SOURCE_DIR}/StecaInstallScript.qs)
