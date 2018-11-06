095a7facd7fc26d17b0dd729de936684ecb70ef4
vagrant <vagrant>
2018-11-06 16:23:36 +0100

    No more need to specify Qt5 module paths one by one.
    The cue was to change lib -> bin in the PATH

--- _StecaBuild64_.bat
+++ _StecaBuild64_.bat
@@ -1,8 +1,5 @@
-set QTDIR=C:\Qt5\5.11.2\msvc2017_64
-set PATH=C:\Qt5\5.11.2\msvc2017_64\lib;C:\yaml.win64\local_x64;C:\Qt5\QtIFW-3.0.4\bin;%PATH%
-set Qt5Core_DIR=C:\Qt5\5.11.2\msvc2017_64\lib\cmake
-set Qt5Gui_DIR=C:\Qt5\5.11.2\msvc2017_64\lib\cmake
-set Qt5Widgets_DIR=C:\Qt5\5.11.2\msvc2017_64\lib\cmake
-set Qt5Network_DIR=C:\Qt5\5.11.2\msvc2017_64\lib\cmake
+set QTDIR=C:\Qt\5.11.2\msvc2017_64
+set PATH=C:\Qt\5.11.2\msvc2017_64\bin;C:\yaml.win64\local_x64;C:\Qt\QtIFW-3.0.4\bin;%PATH%
 cmake -G "Visual Studio 15 2017" -A x64 -T host=x64 .. -DBUILD_SHARED_LIBS=OFF
 cmake --build . --config Release
+cpack
