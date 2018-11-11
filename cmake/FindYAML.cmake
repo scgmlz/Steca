61b513dfa35460b3d1a48c84d79e6d13d681b342
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2018-11-11 22:08:36 +0100

    Unified library names; builds with and w/o fallback libs

--- cmake/FindYAML.cmake
+++ cmake/FindYAML.cmake
@@ -1,13 +1,13 @@
 # CMake module to search for the C library libyaml
 #
-# If the library is found, then LIBYAML_FOUND is set to TRUE,
+# If the library is found, then yaml_FOUND is set to TRUE,
 # and following variables are set:
-#    LIBYAML_INCLUDE_DIR
-#    LIBYAML_LIBRARIES
+#    yaml_INCLUDE_DIR
+#    yaml_LIBRARIES
 
-find_path(LIBYAML_INCLUDE_DIR NAMES yaml.h)
-find_library(LIBYAML_LIBRARIES NAMES yaml libyaml)
+find_path(yaml_INCLUDE_DIR NAMES yaml.h)
+find_library(yaml_LIBRARIES NAMES yaml libyaml)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(yaml DEFAULT_MSG LIBYAML_LIBRARIES LIBYAML_INCLUDE_DIR)
-mark_as_advanced(LIBYAML_INCLUDE_DIR LIBYAML_LIBRARIES)
+find_package_handle_standard_args(yaml DEFAULT_MSG yaml_LIBRARIES yaml_INCLUDE_DIR)
+mark_as_advanced(yaml_INCLUDE_DIR yaml_LIBRARIES)
