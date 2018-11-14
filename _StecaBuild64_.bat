5032cd7cdd686c7ceaed604dbcf0629c3f647a1f
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-11-14 16:49:47 +0100

    "VS 15 2017 Win64" as in BornAgain

--- _StecaBuild64_.bat
+++ _StecaBuild64_.bat
@@ -1,8 +1,6 @@
 set QTDIR=C:\Qt\5.11.2\msvc2017_64
 set PATH=C:\Qt\5.11.2\msvc2017_64\bin;C:\Qt\QtIFW-3.0.4\bin;%PATH%
-# C:\yaml.win64\local_x64;
-cmake -G "Visual Studio 15 2017" -A x64 -T host=x64 ..
-# -DBUILD_SHARED_LIBS=OFF
+cmake -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config Release
 ctest -C Release
 cpack
