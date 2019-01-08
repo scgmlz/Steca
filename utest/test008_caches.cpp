1cce450244a6eda1e6d7c9c3df39c4fed47a49f2
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2019-01-08 10:18:46 +0100

    Simplify simple cache test

--- utest/test008_caches.cpp
+++ utest/test008_caches.cpp
@@ -1,32 +1,27 @@
+//  ***********************************************************************************************
+//
+//  Steca: stress and texture calculator
+//
+//! @file      utest/test008_caches.cpp
+//! @brief     Tests and demonstrates usage of the data cache classes in namespace lazy_data.
+//!
+//! @homepage  https://github.com/scgmlz/Steca
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2016-2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, MAINTAINER)
+//
+//  ***********************************************************************************************
+
 #include "gtest/gtest.h"
 #include "core/typ/lazy_data.h"
 
-class Payload {
-public:
-    Payload(int _x) : x(_x) {}
-    int x;
-};
-
-static int xx;
-
-class Entry {
-public:
-    lazy_data::Cached<Payload> cache{ []()->Payload{ return {xx++}; } };
-};
-
+// Minimal example to test and demonstrate usage of Cached<int>.
 TEST(Caches, OneLevel) {
-    xx = 5;
-
-    Entry e;
-    EXPECT_EQ(5, e.cache.yield().x);
-    EXPECT_EQ(5, e.cache.yield().x);
-    e.cache.invalidate();
-    EXPECT_EQ(6, e.cache.yield().x);
-
-    Entry e2;
-    EXPECT_EQ(7, e2.cache.yield().x);
-    e2.cache.invalidate();
-    EXPECT_EQ(8, e2.cache.yield().x);
-
-    EXPECT_EQ(6, e.cache.yield().x);
+    static int N = 42;
+    auto f = []()->int{ return {N++}; };
+    lazy_data::Cached<int> cache{ f };
+    EXPECT_EQ(42, cache.yield());
+    EXPECT_EQ(42, cache.yield());
+    cache.invalidate();
+    EXPECT_EQ(43, cache.yield());
 }
