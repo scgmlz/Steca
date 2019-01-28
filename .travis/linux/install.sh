e40548c9c9ea15f5f22fddd88ccf41625b01ce1b
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2019-01-28 16:11:18 +0100

    try docker upgrade for travis

--- .travis/linux/install.sh
+++ .travis/linux/install.sh
@@ -1,3 +1,3 @@
-docker build -t bornagain/wily -f `dirname $0`/Dockerfile .
-docker run -d --name dd -it bornagain/wily
+docker build -t bornagain/xenial -f `dirname $0`/Dockerfile .
+docker run -d --name dd -it bornagain/xenial
 docker ps -a
