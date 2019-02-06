b7bc4e452714ce465088c93c2e91ffc3f10a504a
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2019-02-06 21:22:22 +0100

    CMake: try w/o link_directories

--- devtools/script_x64_steca/test.bat
+++ devtools/script_x64_steca/test.bat
@@ -1,3 +1,3 @@
 cd .build
-SET PATH=C:\Qt\5.11.2\msvc2017_64\bin;C:\opt\local_x64\lib;%PATH%;%CD%\bin\Release
+SET PATH=C:\Qt\5.11.2\msvc2017_64\bin;C:\opt\local_x64\lib;%CD%\bin\Release;%PATH%
 ctest -LE Fullcheck -C Release
