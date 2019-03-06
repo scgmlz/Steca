646ec8ffaae5b01225b5ec3f5d095d50df82a9fc
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2019-03-06 21:54:29 +0100

    Move fit models to separate file pair

---
+++ core/fit/fit_models.h
@@ -0,0 +1,48 @@
+//  ***********************************************************************************************
+//
+//  Steca: stress and texture calculator
+//
+//! @file      core/fit/fit_models.h
+//! @brief     Defines fit models
+//!
+//! @homepage  https://github.com/scgmlz/Steca
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2016-2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, MAINTAINER)
+//
+//  ***********************************************************************************************
+
+#ifndef FIT_MODELS_H
+#define FIT_MODELS_H
+
+#include "core/fit/peak_function.h"
+
+//! A Gaussian as a peak fit function.
+
+class Gaussian : public PeakFunction {
+public:
+    void setY(const double* P, const int nXY, const double* X, double* Y) const final;
+    void setDY(const double* P, const int nXY, const double* X, double* Jacobian) const final;
+    int nPar() const final { return 3; }
+};
+
+//! A Lorentzian as a peak fit function.
+
+class Lorentzian : public PeakFunction {
+public:
+    void setY(const double* P, const int nXY, const double* X, double* Y) const final;
+    void setDY(const double* P, const int nXY, const double* X, double* Jacobian) const final;
+    int nPar() const final { return 3; }
+};
+
+//! A Lorentzian as a peak fit function.
+
+class Voigt : public PeakFunction {
+public:
+    void setY(const double* P, const int nXY, const double* X, double* Y) const final;
+    void setDY(const double* P, const int nXY, const double* X, double* Jacobian) const final;
+    int nPar() const final { return 4; }
+    PeakOutcome outcome(const Fitted&) const final;
+};
+
+#endif // FIT_MODELS_H
