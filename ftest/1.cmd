6170a338b797442fde410968a4e57e092bf2e2c0
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2019-03-08 11:05:30 +0100

    restart ftest/

---
+++ ftest/1.cmd
@@ -0,0 +1,53 @@
+[    0ms main log] # Steca 2.1.2 started at 2019-03-07 12:34::49.661
+[   12ms main ini] dfg.all off
+[   52ms main ini] enableCorr on
+[   32ms main ini] linkCuts on
+[   73ms main ini] detDistance 1035
+[   38ms main ini] detPixelSize 1
+[   32ms main ini] beamOffsetI 0
+[   40ms main ini] beamOffsetJ 0
+[   39ms main ini] cutLeft 5
+[   31ms main ini] cutTop 5
+[   36ms main ini] cutBottom 5
+[   38ms main ini] cutRight 5
+[   37ms main ini] dropIncomplete off
+[   44ms main ini] combineMeasurements 2
+[   34ms main ini] numSlices 4
+[   40ms main ini] degree 2
+[   28ms main ini] reflTyp 1
+[   46ms main ini] doInterpol off
+[   42ms main ini] stepAlpha 5
+[   47ms main ini] stepBeta 5
+[   43ms main ini] idwRadius 10
+[   33ms main ini] avgAlphaMax 5
+[   31ms main ini] avgRadius 5
+[   45ms main ini] threshold 100
+[   36ms main ini] idxMeas 1
+[   41ms main ini] idxTheta 1
+[   43ms main ini] idxSlice 1
+[   50ms main ini] diagramCoordX 17
+[   40ms main ini] diagramCoordY 6
+[   35ms main ini] gridPts off
+[   36ms main ini] normTyp 4
+[   41ms main ini] intenAvg on
+[   39ms main ini] intenScale 1
+[        main gui] addFiles
+[        fdia gui] fdia select in/m280580.dat
+[        fdia gui] @close
+[    9ms main gui] ## Long time task began:  "addFiles"
+[  402ms main gui] ## Long time task ended:  "addFiles"
+[        main gui] setupTabs 2 # Peakfits
+[        main gui] dfgram add 45.6 47.55
+[        main gui] mainTabs 3 # Diagram
+[   17ms main gui] ## Long time task began:  "peak fitting"
+[  635ms main gui] ## Long time task ended:  "peak fitting"
+[        main gui] diagramCoordY 4 # intensity
+[        main gui] diagramCoordX 15 # φ
+[        main gui] exportDiagram
+[        modal gui] file out/dia1
+[        modal gui] save
+[        modal gui] @close
+[        main gui] quit
+[   83ms main log] # Steca session ended
+[    0ms main log] # duration: 54802ms
+[    0ms main log] # computing time: 2400ms
