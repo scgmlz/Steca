fbac491ba9b7b57abc6ef8dcb5a80f862ab4e448
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2019-03-14 21:54:09 +0100

    c'tor calls {} <- ()

--- gui/view/plot_dfgram.cpp
+++ gui/view/plot_dfgram.cpp
@@ -40,11 +40,11 @@ QColor scatter {255, 0, 0};
 
 //! Equips PlotOverlay with domain-specific colors and setter functions.
 
-class PlotDfgramOverlay : public QcrRegistered,public PlotOverlay  {
+class PlotDfgramOverlay : public QcrRegistered, public PlotOverlay  {
 public:
     PlotDfgramOverlay(class PlotDfgram& parent)
-        : QcrRegistered {"dfgram"}
-        , PlotOverlay {parent, RangeControl::STEP} {}
+        : QcrRegistered{"dfgram"}
+        , PlotOverlay{parent, RangeControl::STEP} {}
 private:
     void setFromCommand(const QString&) final;
     void addRange(const Range&) final;
@@ -55,7 +55,7 @@ private:
 
 void PlotDfgramOverlay::addRange(const Range& range)
 {
-    gLogger->log(QString("dfgram add %1 %2").arg(range.min).arg(range.max));
+    gLogger->log(QString{"dfgram add %1 %2"}.arg(range.min).arg(range.max));
 
     // is it a valid range?
     const auto datapointCount = gSession->currentOrAvgeDfgram()->curve.intersect(range).size();
@@ -90,7 +90,7 @@ void PlotDfgramOverlay::addRange(const Range& range)
 
 void PlotDfgramOverlay::selectRange(double x)
 {
-    gLogger->log(QString("dfgram sel %1").arg(x));
+    gLogger->log(QString{"dfgram sel %1"}.arg(x));
     bool selectionChanged = false;
     // prioritize baseline sel. when editing baselines
     if (gSession->params.editableRange == EditableRange::BASELINE)
