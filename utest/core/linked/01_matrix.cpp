4a36176b5bf45142a5cde48e0886986dbffe896e
paulnbn <paul.cuccuru@imt-atlantique.net>
2019-06-25 10:26:22 +0200

    Refactor MatrixTest - BasicOperations with catch2

--- utest/core/linked/01_matrix.cpp
+++ utest/core/linked/01_matrix.cpp
@@ -1,5 +1,4 @@
 #define CATCH_CONFIG_MAIN
-#include "gtest/gtest.h"
 #include "core/calc/matrix.h"
 #include "3rdparty/catch2/catch.hpp"
 
@@ -9,40 +8,13 @@ TEST_CASE( "MatrixTest - BasicOperations", "[01_matrix]" ) {
     mat3r mt(0, 3, 6, 1, 4, 7, 2, 5, 8);
     mat3r mc(m1);
 
-    CHECK(3<2); // 1st test
-
     CHECK(m1.transposed() == mt);
     CHECK(m1 == mc);
     CHECK(mt.transposed() == m1);
 
-    CHECK(10==9); // 2nd test
-
     CHECK(vec3r(17, 62, 107) == m1 * vec3r(4, 5, 6));
     CHECK(mat3r(5, 14, 23, 14, 50, 86, 23, 86, 149) == m1 * mt);
-
-    CHECK(1>2); // 3rd test
-
 }
-
-
-//TEST(MatrixTest, BasicOperations) {
-//    mat3r m1(0, 1, 2, 3, 4, 5, 6, 7, 8);
-//    mat3r mt(0, 3, 6, 1, 4, 7, 2, 5, 8);
-//    mat3r mc(m1);
-
-//    EXPECT_LT(3,2);
-
-//    EXPECT_EQ(m1.transposed(), mt);
-//    EXPECT_EQ(m1, mc);
-//    EXPECT_EQ(mt.transposed(), m1);
-
-//    EXPECT_EQ(10,9);
-
-//    EXPECT_EQ(vec3r(17, 62, 107), m1 * vec3r(4, 5, 6));
-//    EXPECT_EQ(mat3r(5, 14, 23, 14, 50, 86, 23, 86, 149), m1 * mt);
-
-//    EXPECT_GT(1, 2); // 3rd test
-//}
 
 
 /*
